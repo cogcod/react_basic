32db6cb6658f7ce0088d2296e3743ee48bbe52b3
cogcod <yricog1290@gmail.com>
2022-08-19 09:00:36 +0900

    input의 값을 event.target.value로 선택해서 state 변경

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -8,12 +8,36 @@ import {useState} from "react";
 // }
 
 function Button() {
-let [ count, setCount ] = useState(0);
+    // const [ count, setCount ] = useState(0);
+    // const onClick = () => {
+    //     setCount((current) => current + 1);  // 자동으로 counter 수정
+    // }
+
+    const [ minutes, setMinutes ] = useState();
+    const onChange = (event) => {
+        setMinutes(event.target.value)
+    }
+
 
     return (
         <div>
-            <h3>Total clicks: {count}</h3>
-            <button onClick={()=> setCount(count + 1)}>Click me</button>
+            <h1>Super Converter</h1>
+            {/*<button onClick={ setCount((current) => current + 1) }>Click me</button>*/}
+            {/* setCount에는 변경할 값을 직접적으로 넣을 수도 있고, 함수도 넣을 수 있다. */}
+
+            <label for="minutes">Minutes</label>
+            <input
+                value={minutes}
+                id="minutes"
+                placeholder="Minutes"
+                type="number"
+                onChange={onChange}
+            />
+
+            <div>You want to convert {minutes}</div>
+
+            <label for="hours">Hours</label>
+            <input id="hours" placeholder="Hours" type="number"/>
         </div>
     );
 }
