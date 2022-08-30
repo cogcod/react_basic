dc9e111fb1788667a1c52108f7f509e38b6a31e6
cogcod <yricog1290@gmail.com>
2022-08-30 17:43:21 +0900

    콘솔창에 두줄씩 찍히는 에러 해결

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
+  /*
+    StricMode : 리액트에서 제공하는 검사도구 (배포 후 문제가 될만한 이슈들을 미리 집아줌)
+    이 태그로 App을 감싸면 개발모드에서 오류를 잡기 위해 두번씩 렌더링 된다.
+   */
 );
