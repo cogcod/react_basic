d4b37dd7bf9f34ef9810302e32d2d3392ab14d70
cogcod <yricog1290@gmail.com>
2022-09-03 18:43:59 +0900

    MovieApp 2. async, await을 통해 불러오기 - 첫번째 방법

--- src/movieApp/MovieApp.js
+++ src/movieApp/MovieApp.js
@@ -4,13 +4,17 @@ function MovieApp() {
     const [loading, setLoading] = useState(true)
     const [movies, setMovies] = useState([])
 
+    const getMovies = async() => {
+        const response = await fetch(
+            "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+        );
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
+
     useEffect(()=> {
-        fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
-            .then((response) => response.json())
-            .then((json) =>
-                setMovies(json.data.movies),
-                setLoading(false)
-            )
+        getMovies();
     },[])
 
     console.log(movies)
