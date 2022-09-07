e090d1f4f75f0c376a8a40f333f134d32a610431
cogcod <yricog1290@gmail.com>
2022-09-07 14:24:03 +0900

    새 페이지 이동 연습

--- src/movieApp/components/DetailList.js
+++ src/movieApp/components/DetailList.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function DetailList({title, description_full, medium_cover_image}) {
+    const src = "https://www.naver.com/";
+
+    const onClick = () => {
+        window.location.href = `${src}`
+    }
     return (
         <div>
             <h2>{title}</h2>
             <img src={medium_cover_image} alt={title} />
             <p>{description_full}</p>
+            <button onClick={onClick}>새 페이지 이동 연습</button>
         </div>
     );
 }
